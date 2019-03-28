0acd57ce32733332d134070343c128be6bf2652b
Alberto <rodelgo7al@gmail.com>
2019-03-28 14:16:11 +0100

    Edited folder names and standard text

--- src/include/alphanumeric_viewer_process_main.h
+++ src/include/alphanumeric_viewer_process_main.h
@@ -1,6 +1,32 @@
 #ifndef ALPHANUMERIC_VIEWER_ROSMODULE_H
 #define ALPHANUMERIC_VIEWER_ROSMODULE_H
 
+/*!*************************************************************************************
+ *  \process   Alphanumeric_viewer
+ *
+ *  \brief     Aphanumeric viewer of aerial robotic parameters.
+ *
+ *  \details   This process shows data related to sensor measurements, self localization,
+ *             actuator commands and motion references.
+ *             It doesn't publish messages to any topic.
+ *
+ *  \authors   Alberto Rodelgo
+ *
+ *  \copyright Copyright 2019 Universidad Politecnica de Madrid (UPM)
+ *  
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *  
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *  
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program. If not, see http://www.gnu.org/licenses/.
+ ***************************************************************************************/
 
 //STD CONSOLE
 #include <sstream>
